8dcc831a3cd70643492a7adbfa19112258d331a1
Moses-Morris <mosesmorrisdev@gmail.com>
2023-09-05 00:05:19 -0700

    Start Redis and Mongo DB.

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,60 @@
-import express from 'express';
+const express = require('express');
+const router = express.Router();
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+
+router.use(express.json());
 
 function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
+	  const router = express.Router();
+	  app.use('/', router);
 
-  // App Controller
+	  router.get('/status', (req, res) => {
+		      AppController.getStatus(req, res);
+		    });
 
-  // should return if Redis is alive and if the DB is alive
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+	  router.get('/stats', (req, res) => {
+		      AppController.getStats(req, res);
+		    });
 
-  // should return the number of users and files in DB
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+	  router.post('/users', (req, res) => {
+		      UsersController.postNew(req, res);
+		    });
 
-  // User Controller
+	  router.get('/connect', (req, res) => {
+		      AuthController.getConnect(req, res);
+		    });
 
-  // should create a new user in DB
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
+	  router.get('/disconnect', (req, res) => {
+		      AuthController.getDisconnect(req, res);
+		    });
 
-  // should retrieve the user base on the token used
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
+	  router.get('/users/me', (req, res) => {
+		      UsersController.getMe(req, res);
+		    });
 
-  // Auth Controller
+	  router.post('/files', (req, res) => {
+		      FilesController.postUpload(req, res);
+		    });
 
-  // should sign-in the user by generating a new authentication token
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
+	  router.get('/files/:id', (req, res) => {
+		      FilesController.getShow(req, res);
+		    });
 
-  // should sign-out the user based on the token
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
+	  router.get('/files', (req, res) => {
+		      FilesController.getIndex(req, res);
+		    });
 
-  // Files Controller
+	  router.put('/files/:id/publish', (req, res) => {
+		      FilesController.putPublish(req, res);
+		    });
 
-  // should create a new file in DB and in disk
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
+	  router.put('/files/:id/unpublish', (req, res) => {
+		      FilesController.putUnpublish(req, res);
+		    });
 
-  // should retrieve the file document based on the ID
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
-
-  // should retrieve all users file documents for a
-  // specific parentId and with pagination
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
-
-  // should set isPublic to true on the file document based on the ID
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
-
-  // should set isPublic to false on the file document based on the ID
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
-
-  // should return the content of the file document based on the ID
-  router.get('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
+	  router.get('/files/:id/data', (req, res) => {
+		      FilesController.getFile(req, res);
+		    });
 }
 
 export default controllerRouting;
